1855ece1fa74d6a500b274126b5969e9a1a733ab
aldaffaafino <aldaffaafino@gmail.com>
2020-10-03 11:23:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myintentapp/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+    }
